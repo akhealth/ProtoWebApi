90c8302f50e709f7e20209058d2ea99af5a46ef2
Clinton Troxel <clinton.troxel@gsa.gov>
2017-09-19 18:56:08 +0000

    Merged PR 67: HTTP Basic auth support
    HTTP Basic auth support
    
    Thanks to @<mark.headd@gsa.gov> for figuring out the easiest and best way to add HTTP Basic auth!

--- AKRestAPI/Connectors/SoapConnector.cs
+++ AKRestAPI/Connectors/SoapConnector.cs
@@ -1,44 +1,58 @@
+using System;
+using System.Net.Http;
+using System.Text;
 using System.Xml;
 using SoapHttpClient;
 using SoapHttpClient.Extensions;
 
 namespace AK.connectors
 {
-    public class SoapConnector
+  public class SoapConnector
+  {
+    // URL for SOAP endpoint.
+    private string _endpoint;
+
+    /*
+     * Class constructor
+     */
+    public SoapConnector(string endpoint)
     {
-        // URL for SOAP endpoint.
-        private string _endpoint;
-
-        /*
-         * Class constructor
-         */
-        public SoapConnector(string endpoint)
-        {
-            _endpoint = endpoint;
-        }
-
-        /*
-         * Execute SOAP call with payload.
-         */
-        public XmlDocument makeCall(string payload)
-        {
-
-            // XML doc to hold the SOAP payload.
-            XmlDocument body = new XmlDocument();
-            body.LoadXml(payload);
-
-            using (SoapClient soapClient = new SoapClient())
-            {
-                // Make call to SOAP endpoint.
-                var result = soapClient.Post(endpoint: _endpoint, body: body);
-
-                // XML doc to hold SOAP response.
-                XmlDocument response = new XmlDocument();
-                response.LoadXml(result.Content.ReadAsStringAsync().Result);
-
-                // Return XML from SOAP service.
-                return response;
-            }
-        }
+      _endpoint = endpoint;
     }
+
+    /*
+     * Execute SOAP call with payload.
+     */
+    public XmlDocument makeCall(string payload)
+    {
+
+      // XML doc to hold the SOAP payload.
+      XmlDocument body = new XmlDocument();
+      body.LoadXml(payload);
+
+      using (SoapClient soapClient = new SoapClient(httpClientFactory))
+      {
+        // Make call to SOAP endpoint.
+        var result = soapClient.Post(endpoint: _endpoint, body: body);
+
+        // XML doc to hold SOAP response.
+        XmlDocument response = new XmlDocument();
+        response.LoadXml(result.Content.ReadAsStringAsync().Result);
+
+        // Return XML from SOAP service.
+        return response;
+      }
+    }
+
+    // Configure HTTP client with Basic auth creds.
+    private HttpClient httpClientFactory()
+    {
+      HttpClient client = new HttpClient();
+      var _userid = AKRestAPI.Util.GetEnv("SoapHttpBasicUser");
+      var _password = AKRestAPI.Util.GetEnv("SoapHttpBasicPass");
+      var byteArray = Encoding.ASCII.GetBytes(String.Format(@"{0}:{1}", _userid, _password));
+      client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+      return client;
+    }
+  }
 }
